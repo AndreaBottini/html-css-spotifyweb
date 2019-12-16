9228d87d67c63a5901eeb07cc25030a0a960328e
Andrea Bottini <andrea.bottini22@gmail.com>
2019-12-16 18:58:34 +0100

    Prova cambio titolo!!!

--- javascript/js/myscript.js
+++ javascript/js/myscript.js
@@ -1,1 +1,3 @@
-alert('Clicca ok per accedere alla mia copia di Spotify!');
+//alert('Clicca ok per accedere alla mia copia di Spotify!');
+
+document.getElementById('title').innerHTML='Ho cambiato il titolo e ha funzionato!!';
